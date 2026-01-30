1c9614d6a82afcb31ab1e7983cce47dea8b76752
Gabriel Kollet <gabrielkollet99@gmail.com>
2026-01-29 21:09:10 -0300

    test: simultaneous requests
    plus asserting "statement.desc" matches "desc" on request body.

--- tests/integration/transaction_test.py
+++ tests/integration/transaction_test.py
@@ -36,7 +36,7 @@ async def test_credit_success(db_pool, client):
     assert new_transaction is not None
     assert new_transaction['value'] == req_body['value']
     assert new_transaction['type'] == req_body['type']
-    assert new_transaction['description'] == req_body['desc']
+    assert new_transaction['desc'] == req_body['desc']
 
 
 async def test_credit_failure(db_pool, client):
@@ -79,7 +79,7 @@ async def test_debit_success(db_pool, client):
     assert new_transaction is not None
     assert new_transaction['value'] == req_body['value']
     assert new_transaction['type'] == req_body['type']
-    assert new_transaction['description'] == req_body['desc']
+    assert new_transaction['desc'] == req_body['desc']
 
 
 async def test_debit_failure(db_pool, client):
@@ -100,3 +100,36 @@ async def test_debit_failure(db_pool, client):
 
     new_transaction = await db_pool.fetchrow(Queries.TRANSACTION)
     assert new_transaction is None
+
+
+async def test_debit_concurrently_fail(db_pool, client):
+    req_body = { 'value': 100, 'type': 'd', 'desc': 'desc' }
+
+    res = await asyncio.gather(*[
+        client.post('/customers/1/transaction', json=req_body)
+        for _ in range(1001)
+    ])
+    assert sum([1 for r in res if r.status_code == 200]) == 1000
+    assert sum([1 for r in res if r.status_code == 422]) == 1
+
+    balance_after = await db_pool.fetchval(Queries.BALANCE)
+    overdraft_limit = await db_pool.fetchval(Queries.OVERDRAFT)
+    assert balance_after >= overdraft_limit*(-1)
+
+
+async def test_zero_sum_concurrently(db_pool, client):
+    req_body_c = { 'value': 100, 'type': 'c', 'desc': 'desc' }
+    req_body_d = { 'value': 100, 'type': 'd', 'desc': 'desc' }
+
+    balance_before = await db_pool.fetchval(Queries.BALANCE)
+
+    res = await asyncio.gather(
+        *[client.post('/customers/1/transaction', json=req_body_c)
+          for _ in range(1000)],
+        *[client.post('/customers/1/transaction', json=req_body_d)
+          for _ in range(1000)]
+    )
+    assert all([r.status_code == 200 for r in res])
+
+    balance_after = await db_pool.fetchval(Queries.BALANCE)
+    assert balance_before == balance_after
